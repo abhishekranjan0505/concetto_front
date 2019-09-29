8bd5e70105c136097d0d2c5ab78c51070459971b
abhi0505 <abhishekranjan0505@gmail.com>
2019-09-29 23:10:49 +0530

    eventDetail

--- src/components/EventDetail/index.js
+++ src/components/EventDetail/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { UncontrolledCarousel } from 'reactstrap';
+// import Image from "reactstrap";
 import "./Styles.css";
 import Loading from "../Loading";
 import About from "./About";
@@ -44,26 +44,26 @@ state={
   }
 
   render() {
-    const items = [
-      {
-        src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-        altText: 'Slide 1',
-        caption: 'Slide 1',
-        header: 'Slide 1 Header'
-      },
-      {
-        src: 'http://farm9.staticflickr.com/8504/8365873811_d32571df3d_z.jpg',
-        altText: 'Slide 2',
-        caption: 'Slide 2',
-        header: 'Slide 2 Header'
-      },
-      {
-        src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-        altText: 'Slide 3',
-        caption: 'Slide 3',
-        header: 'Slide 3 Header'
-      }
-    ];
+    // const items = [
+    //   {
+    //     src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+    //     altText: 'Slide 1',
+    //     caption: 'Slide 1',
+    //     header: 'Slide 1 Header'
+    //   },
+    //   {
+    //     src: 'http://farm9.staticflickr.com/8504/8365873811_d32571df3d_z.jpg',
+    //     altText: 'Slide 2',
+    //     caption: 'Slide 2',
+    //     header: 'Slide 2 Header'
+    //   },
+    //   {
+    //     src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+    //     altText: 'Slide 3',
+    //     caption: 'Slide 3',
+    //     header: 'Slide 3 Header'
+    //   }
+    // ];
     const event = {
       id: 1,
       name: "Event 1",
@@ -99,10 +99,10 @@ state={
             <img src="assets/logo.png" className="img-fluid" alt={Loading} />
           </div>
           <div className="btnFlex">
-            <button className={"btn btn-2 "+((this.state.active==='about')?'active-bottom':'')} onClick={this.aboutShow}>ABOUT</button>
-            <button className={"btn btn-2 "+((this.state.active==='rules')?'active-bottom':'')} onClick={this.rulesShow}>RULES</button>
-            <button className={"btn btn-2 "+((this.state.active==='details')?'active-bottom':'')} onClick={this.detailsShow}>DETAILS</button>
-            <button className={"btn btn-2 "+((this.state.active==='register')?'active-bottom':'')} onClick={this.registerShow}>REGISTER</button>
+            <button className={"btn btn-event btn-2 "+((this.state.active==='about')?'active-bottom':'')} onClick={this.aboutShow}>ABOUT</button>
+            <button className={"btn btn-event btn-2 "+((this.state.active==='rules')?'active-bottom':'')} onClick={this.rulesShow}>RULES</button>
+            <button className={"btn btn-event btn-2 "+((this.state.active==='details')?'active-bottom':'')} onClick={this.detailsShow}>DETAILS</button>
+            <button className={"btn btn-event btn-2 "+((this.state.active==='register')?'active-bottom':'')} onClick={this.registerShow}>REGISTER</button>
           </div>
         </div>
         <div style={{display: 'flex', flexDirection: 'column'}}>
@@ -146,7 +146,12 @@ state={
               <label for="img-2" className="nav-dot" id="img-dot-2"></label>
             </li>
           </ul> */}
-                <UncontrolledCarousel items={items} />
+                {/* <UncontrolledCarousel items={items} /> */}
+          <img
+          src="http://farm9.staticflickr.com/8504/8365873811_d32571df3d_z.jpg"
+          alt={event.name}
+          fluid
+          />
         {/* <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
           <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
