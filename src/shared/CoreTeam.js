9857c4db95eab7ec179bb5449cf0318f3684b33e
cjchirag7 <cjchirag7@gmail.com>
2019-10-02 01:22:32 +0530

    Added scroll to (0,0) on componentDidMount

---
+++ src/shared/CoreTeam.js
@@ -0,0 +1,26 @@
+export const CoreTeam = [
+  {
+    name: 'Neelansh Maheshwari',
+    post: 'Technical Head',
+    pic: ''
+  },
+  {
+    name: 'Sirigireddy Dhana Laxmi',
+    post: 'Technical Team',
+    pic: ''
+  },
+  {
+    name: 'Akshay Kadam',
+    post: 'Sponsorship Team',
+    pic: ''
+  },
+  {
+    name: 'Neelansh Maheshwari',
+    post: 'Technical Head',
+    pic: ''
+  },
+  {
+    name: 'Neelansh Maheshwari',
+    post: 'Technical Head',
+    pic: ''
+  },
