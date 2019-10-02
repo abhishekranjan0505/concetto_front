b43a92aef7f8e55383f5838978bb72f2b8867c43
abhi0505 <abhishekranjan0505@gmail.com>
2019-10-03 02:09:31 +0530

    eventDetail

--- src/components/EventDetail/About.js
+++ src/components/EventDetail/About.js
@@ -4,7 +4,7 @@ class About extends Component {
   render() {
     const { event } = this.props;
     return (
-      <div className="container">
+      <div className="container" style={{ display: "block", width: "80vw" }}>
         {" "}
         {` About:- ${event.description + "\n" + event.about} \n `}
         <br />
--- src/components/EventDetail/Details.js
+++ src/components/EventDetail/Details.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class Details extends Component {
   render() {
     const { event } = this.props;
-    return <div className="container">{event.description}</div>;
+    return <div className="container" style={{ display: "block", width: "80vw" }}>
+    {event.description}
+    </div>;
   }
 }
 
