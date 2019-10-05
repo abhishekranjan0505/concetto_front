8bab1c6fecc2c632a01338a44ed3da06d95efb03
cjchirag7 <cjchirag7@gmail.com>
2019-10-06 00:25:32 +0530

    Properly spaced the details of events

--- src/shared/CoreTeam.js
+++ src/shared/CoreTeam.js
@@ -7,7 +7,7 @@ export const CoreTeam = [
   },
   {
     name: "Ashutosh Jindal",
-    post: "Co-Cordinator",
+    post: "Co-Coordinator",
     pic:
       "https://concetto-front.s3.ap-south-1.amazonaws.com/core-team/Ashutosh+Jindal-+Co-Coordinator.jpg"
   },
@@ -28,6 +28,12 @@ export const CoreTeam = [
     post: "Security Head",
     pic:
       "https://concetto-front.s3.ap-south-1.amazonaws.com/core-team/Tarun+Kumar+security+(head).jpg"
+  },
+  {
+    name: "Vislavath Shirisha",
+    post: "Exhibition Coordinator",
+    pic:
+      "https://concetto-front.s3.ap-south-1.amazonaws.com/core-team/Sirisha+.jpg"
   },
   {
     name: "Sirigireddy Dhana Laxmi",
@@ -82,12 +88,6 @@ export const CoreTeam = [
     post: "Promotion Team",
     pic:
       "https://concetto-front.s3.ap-south-1.amazonaws.com/core-team/Shweta+Suman.jpg"
-  },
-  {
-    name: "Vislavath Shirisha",
-    post: "Sponsership Team",
-    pic:
-      "https://concetto-front.s3.ap-south-1.amazonaws.com/core-team/Sirisha+.jpg"
   },
   {
     name: "Keshav Kumar Singh",
